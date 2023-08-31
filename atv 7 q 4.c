ee8ae60a4de556f31a353dc4d22db592b2373052
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2023-08-31 19:49:22 -0300

    Add files via upload

---
+++ atv 7 q 4.c
@@ -0,0 +1,23 @@
+//atv 7 q 4
+#include <stdio.h>
+#include <stdlib.h>
+void funcao(){
+    int A,B,vA,vB;
+    scanf("%d",&A);
+    printf("A=%d\n",A);
+    scanf("%d",&B);
+    printf("B=%d\n\n",B);
+    vA = A;
+    vB = B;
+    A = vB;
+    B = vA;
+    printf("A=%d \nB=%d\n",A,B);
+    return;
+}
+
+int main(){
+    
+    funcao();
+    
+    return 0;
+}
